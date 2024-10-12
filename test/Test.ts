5b93e26f156ca52a2dd6c4889533727e6097a808
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-10-12 17:53:48 +0100

    test script added; deployment test done

---
+++ test/Test.ts
@@ -0,0 +1,66 @@
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { expect } from 'chai';
+import hre from 'hardhat';
+
+describe('Deployment Tests', function () {
+  // We define a fixture to reuse the same setup in every test.
+  async function deployContractsFixture() {
+    const [owner] = await hre.ethers.getSigners();
+
+    // Deploy AcceptedTokens contract
+    const AcceptedTokens = await hre.ethers.getContractFactory(
+      'AcceptedTokens'
+    );
+    const acceptedTokens = await AcceptedTokens.deploy();
+
+    // Deploy Escrow contract
+    const feePercentage = 5;
+    const Escrow = await hre.ethers.getContractFactory('Escrow');
+    const escrow = await Escrow.deploy(feePercentage);
+
+    // Deploy AsoEbiAuction contract
+    const AsoEbiAuction = await hre.ethers.getContractFactory('AsoEbiAution');
+    const asoEbiAuction = await AsoEbiAuction.deploy(escrow.getAddress());
+
+    // Deploy OwnerShip contract
+    const OwnerShip = await hre.ethers.getContractFactory('OwnerShip');
+    const ownerShip = await OwnerShip.deploy();
+
+    return {
+      acceptedTokens,
+      escrow,
+      asoEbiAuction,
+      ownerShip,
+      owner,
+      feePercentage,
+    };
+  }
+
+  describe('Deployment', function () {
+    it('Should deploy the AcceptedTokens contract and set the right owner', async function () {
+      const { acceptedTokens, owner } = await loadFixture(
+        deployContractsFixture
+      );
+      expect(await acceptedTokens.owner()).to.equal(owner.address);
+    });
+
+    it('Should deploy the Escrow contract and set the right fee percentage', async function () {
+      const { escrow, feePercentage } = await loadFixture(
+        deployContractsFixture
+      );
+      expect(await escrow.feePercentage()).to.equal(feePercentage);
+    });
+
+    // it('Should deploy the AsoEbiAuction contract and set the right escrow address', async function () {
+    //   const { asoEbiAuction, escrow } = await loadFixture(
+    //     deployContractsFixture
+    //   );
+    //   expect(await asoEbiAuction.escrowAddress()).to.equal(escrow.getAddress());
+    // });
+
+    it('Should deploy the OwnerShip contract and set the right owner', async function () {
+      const { ownerShip, owner } = await loadFixture(deployContractsFixture);
+      expect(await ownerShip.owner()).to.equal(owner.address);
+    });
+  });
+});
